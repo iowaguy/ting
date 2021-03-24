f3f18e88e8c0ffd66c146dcbbc6da0619600d5a6
Ben Weintraub <ben@weintraub.xyz>
2021-03-24 17:57:14 -0400

    some thoughts

--- ting/ting.py
+++ ting/ting.py
@@ -2,10 +2,11 @@
 of internals."""
 
 import logging
+import threading
 from typing import Tuple, List, Dict, Union
 
 from ting.client import TingClient
-from ting.utils import Fingerprint, IPAddress, RelayPair
+from ting.utils import Fingerprint, IPAddress, RelayPair, TingLeg
 
 
 def ting(
@@ -16,20 +17,30 @@ def ting(
     num_samples: int = 10,
     local_test: bool = False,
     **kwargs: Union[str, int],
-) -> Dict[RelayPair, Dict[str, List[float]]]:
+) -> Dict[RelayPair, Dict[TingLeg, List[float]]]:
     """A high-level interface for Ting."""
+
+    # problem with the following approach: zach will basically need to reimplement all this threading stuff; how can i hide it?
+    # create Event(), e
+    # start echo_server thread, passing it e
+    # also pass e to client
+    # during sample, client: starts timer, sends message, and waits for event
+    # once event occurs, stop timer and record, then return
+
+
+    # when echo server receives message, it flags event (e.set())
+
     ting_client = TingClient(relay_w_fp, relay_z_fp, source_addr, local_test, **kwargs)
 
-    results: Dict[RelayPair, Dict[str, List[float]]] = dict()
+    results: Dict[RelayPair, Dict[TingLeg, List[float]]] = dict()
     logging.info("Measure RTT between the following nodes: %s", measurement_targets)
     for relay1, relay2 in measurement_targets:
-        results[(relay1, relay2)] = {"x": [], "y": [], "xy": []}
+        results[(relay1, relay2)] = {TingLeg.X: [], TingLeg.Y: [], TingLeg.XY: []}
         circuit_templates = ting_client.generate_circuit_templates(relay1, relay2)
         for circuit_template in circuit_templates.all:
-            # circuit_results: Dict[str, List[float]] = {circuit_template.leg.value: []}
             with circuit_template as circuit:
                 for _ in range(num_samples):
-                    results[(relay1, relay2)][circuit_template.leg.value].append(
+                    results[(relay1, relay2)][circuit_template.leg].append(
                         circuit.sample()
                     )
 
