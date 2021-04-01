23cfc111ae6a13f1380fa656598233177765d997
Ben Weintraub <ben@weintraub.xyz>
2021-04-01 10:46:51 -0400

    [echo] Move echo_server to within the ting process

--- ting.sh
+++ ting.sh
@@ -8,7 +8,7 @@ NETWORK_SPECS=${CHUTNEY_PATH}/networks/basic
 CHUTNEY_NODES=${CHUTNEY_PATH}/net/nodes
 TING_CONFIG_FILE=tingrc
 SCRIPTS=./scripts
-TING_LOG_LEVEL=WARNING
+TING_LOG_LEVEL=DEBUG
 TOR_DATA=${SCRIPTS}/data
 TOR_DATA_W=${TOR_DATA}/w
 TOR_DATA_Z=${TOR_DATA}/z
@@ -73,11 +73,11 @@ MaxCircuitBuildAttempts 5
 EOF
 
   grep DirAuthority ${CHUTNEY_PATH}/net/nodes/000a/torrc >> ./${TING_CONFIG_FILE}
-  ${SCRIPTS}/echo_server &
+  # ${SCRIPTS}/echo_server &
 
   ${SCRIPTS}/ting_runner.py --log-level ${TING_LOG_LEVEL} ${fingerprint_relay_x} ${fingerprint_relay_y}
 
-  kill_echo_server
+  # kill_echo_server
 }
 
 stop_test() {
--- ting/echo_server.py
+++ ting/echo_server.py
@@ -5,8 +5,10 @@
 import logging
 import select
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from threading import Event
+import time
 
-from ting.utils import IPAddress
+from ting.utils import IPAddress, Port
 
 
 class EchoServer:
@@ -15,9 +17,14 @@ class EchoServer:
     __MESSAGE_SIZE = 3
     __TIMEOUT = 0.5
 
-    def __init__(self, host: IPAddress = "0.0.0.0", port: int = 16667) -> None:
+    def __init__(
+        self, event: Event, host: IPAddress = "0.0.0.0", port: Port = 16667
+    ) -> None:
+        self.__logger = logging.getLogger(__name__)
         self.echo_socket = self.__setup_socket(host, port)
         self.running = False
+        self.__event = event
+
 
     def run(self) -> None:
         """Start the echo server."""
@@ -25,34 +32,41 @@ class EchoServer:
         read_list = [self.echo_socket]
         while True:
             readable, _, _ = select.select(read_list, [], [], EchoServer.__TIMEOUT)
-            logging.debug("Socket is ready to read")
+            self.__logger.debug("Socket is ready to read")
             for sock in readable:
                 if sock is self.echo_socket:
                     client_socket, address = self.echo_socket.accept()
                     read_list.append(client_socket)
-                    logging.info("Connection accepted from %s", str(address))
+                    self.__logger.info("Connection accepted from %s", str(address))
                 else:
                     data = sock.recv(EchoServer.__MESSAGE_SIZE)
-                    logging.debug("data recieved=%s", data)
-                    while data and (str(bytes("!c", "utf-8") + data)) != "X":
-                        logging.debug("Echoing data: %s", data)
-                        sock.send(data)
-                        data = sock.recv(EchoServer.__MESSAGE_SIZE)
-                    sock.close()
-                    read_list.remove(sock)
-                    logging.info("Connection closed.")
+                    self.__logger.debug("Data received: %s", data)
+                    if not data or (str(bytes("!c", "utf-8") + data)) == "X":
+                        self.__logger.debug("Client is closing connection.")
+                        sock.close()
+                        read_list.remove(sock)
+                        self.__logger.info("Connection closed.")
+                        continue
+
+                    # If we're here, then the client is tinging us
+                    self.__event.set()
+                    self.__logger.debug("Echoing data: %s", data)
+                    start_time = time.time()
+                    sock.send(data)
+                    self.__event.wait()
+                    stop_time = time.time()
+                    # TODO report stop_time - start_time to main thread
 
     def is_running(self) -> bool:
         """Return True if echo server is running locally."""
         return self.running
 
-    @classmethod
-    def __setup_socket(cls, host: IPAddress, port: int) -> socket:
+    def __setup_socket(self, host: IPAddress, port: Port) -> socket:
         sock = socket(AF_INET, SOCK_STREAM)
         sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         sock.bind((host, port))
 
         backlog = 1
         sock.listen(backlog)
-        logging.info("TCP echo server listening on port %i", port)
+        self.__logger.info("TCP echo server listening on port %i", port)
         return sock
